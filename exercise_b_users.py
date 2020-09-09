9093d5daed4050f6c13d1cdaafd7371fcadf917c
CatAnderson <thecatanderson100@gmail.com>
2020-09-09 15:12:02 +0100

    Question 6 - in class

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -68,10 +68,20 @@ users = {
 
 # 5. Get the smallest of Erik's lottery numbers
 ##print(users["Erik"]["lottery_numbers"][2]) - PRESUME THIS IS TOO SIMPLE
-def lowest_number(lottery_numbers)
-    users(["Erik"])
+
+#def lowest_number(lottery_numbers)
+    #users(["Erik"])
 
 # 6. Return an array of Avril's lottery numbers that are even
+avrils_lottery_numbers = users["Avril"]["lottery_numbers"]
+even_numbers = []
+
+for number in avrils_lottery_numbers:
+  if number % 2 == 0:
+    even_numbers.append(number)
+
+print(even_numbers)
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
